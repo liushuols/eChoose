872748a7a98d1fce6cea85669233883093d7a59b
wangyumiao <1252224324@qq.com>
2018-05-14 08:16:58 +0800

    add

--- 代码/echoose/src/com/bear/echoose/entity/Address.java
+++ 代码/echoose/src/com/bear/echoose/entity/Address.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.bear.echoose.entity;
 
 import java.util.HashSet;
@@ -58,39 +58,4 @@ public class Address {
 	
 
 }
-=======
-//package com.bear.echoose.entity;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import javax.persistence.ManyToMany;
-//
-//public class Address {
-//	private int adid;
-//	private String aprovince;
-//	private Set<Studentuser> studentSet = new HashSet<Studentuser>();
-//	
-//	@ManyToMany(mappedBy="address")
-//
-//	public Set<Studentuser> getStudentSet() {
-//		return studentSet;
-//	}
-//	public void setStudentSet(Set<Studentuser> studentSet) {
-//		this.studentSet = studentSet;
-//	}
-//	public int getAdid() {
-//		return adid;
-//	}
-//	public void setAdid(int adid) {
-//		this.adid = adid;
-//	}
-//	public String getAprovince() {
-//		return aprovince;
-//	}
-//	public void setAprovince(String aprovince) {
-//		this.aprovince = aprovince;
-//	}
-//
-//}
->>>>>>> 416146fcd137f77cace40071a1bd270c105e6c46
+
--- 代码/echoose/src/com/bear/echoose/entity/Collection.java
+++ 代码/echoose/src/com/bear/echoose/entity/Collection.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.bear.echoose.entity;
 
 import javax.persistence.Entity;
@@ -41,53 +41,3 @@ public class Collection {
 
 
 }
-=======
-//package com.bear.echoose.entity;
-//
-//import javax.persistence.CascadeType;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.Table;
-//
-//@Entity
-////@Table(name="collection")
-//
-//
-//public class Collection {
-//	private int id;
-//	private String name;
-//	private Studentuser studentuser;
-//
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
-//
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public Studentuser getStudentuser() {
-//		return studentuser;
-//	}
-//	public void setStudentuser(Studentuser studentuser) {
-//		this.studentuser = studentuser;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//
-//
-//
-//}
->>>>>>> 416146fcd137f77cace40071a1bd270c105e6c46
